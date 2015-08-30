7c5b34f78f9609b72f8c4685daec891b5acb7788
R0uter <lomyr.jo@gmail.com>
2015-08-30 23:06:38 +0800

    Initial Commit

---
+++ TwisterX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TwisterX
+//
+//  Created by R0uter on 15/8/30.
+//  Copyright © 2015年 R0uter. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TwisterX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TwisterX
+//
+//  Created by R0uter on 15/8/30.
+//  Copyright © 2015年 R0uter. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
