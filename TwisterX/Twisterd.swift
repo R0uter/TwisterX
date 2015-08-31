8ab327e54b7cd58ac0126660912e24e6d1993de8
R0uter <lomyr.jo@gmail.com>
2015-08-31 16:46:17 +0800

    The basic model has completed

---
+++ TwisterX/Twisterd.swift
@@ -0,0 +1,50 @@
+//
+//  Shell.swift
+//  TwisterX
+//
+//  Created by R0uter on 15/8/30.
+//  Copyright © 2015年 R0uter. All rights reserved.
+//
+
+import Foundation
+
+
+struct Twisterd {
+   
+    
+    
+    private let task = NSTask()
+    private let pipe = NSPipe()
+    
+    
+    init(launchPath path:String, arguments args:[String]) {
+        task.arguments = args
+        task.launchPath = path
+        task.standardOutput = pipe
+    }
+    
+    func runTwisterd()->String {
+        task.launch()
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output: String = NSString(data: data, encoding: NSUTF8StringEncoding) as! String
+        guard output != "" else { return "Twisterd is started!" }
+        return output
+    }
+    func killTwisterd() {
+        task.terminate()
+    }
+    
+    func restartTwisterd() -> String {
+        task.terminate()
+        while task.running { }
+        task.launch()
+        return "Restarted!"
+    }
+    
+    
+    
+    
+    
+    
+   
+}
--- TwisterX/ViewController.swift
+++ TwisterX/ViewController.swift
@@ -6,14 +6,30 @@
 //  Copyright © 2015年 R0uter. All rights reserved.
 //
 
+
+
+
 import Cocoa
 
 class ViewController: NSViewController {
+    var twisterd:Twisterd!
+    var backgroundQueue = NSOperationQueue()
+    
+    @IBAction func run(sender: AnyObject) {
+        go()
+        backgroundQueue.addOperationWithBlock { twisterd.runTwisterd() }
+        
+    }
+    @IBAction func stop(sender: AnyObject) {
+        twisterd.killTwisterd()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+      
+        
     }
 
     override var representedObject: AnyObject? {
@@ -21,7 +37,16 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
+    func go () {
+        let a:String = NSBundle.mainBundle().bundlePath
+        let twisterdDir = a + "/Contents/Resources/twister/twisterd"
+        let htmldir = "-htmldir=" + a + "/Contents/Resources/twister/html"
+        let proxy = "-proxy=" + "ip:port"
+        let rpcuser = "-rpcuser=" + "user"
+        let rpcpassword = "-rpcpassword" + "pwd"
+        let args = [htmldir,rpcuser,rpcpassword]
+        twisterd = Twisterd(launchPath: twisterdDir, arguments: args )
+    }
 
 }
 
